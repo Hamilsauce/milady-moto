007fec56a33496e4f2641459507e1358dd3b847e
jake hamilton <jacobwilsonhamilton@gmail.com>
2022-12-22 14:30:49 -0600

    store fix

--- src/stores/user.store.ts
+++ src/stores/user.store.ts
@@ -5,6 +5,7 @@ import Web3 from 'web3';
 
 const GCF_LOCAL_URL = 'http://localhost:5000/my-lady-8b48f/us-central1/getMiladyBalance';
 const GCF_URL = 'https://us-central1-my-lady-8b48f.cloudfunctions.net/getMiladyBalance';
+const IAN_WALLET = '0x587376ed782a73966c1b9d9a00635613a6e539dd';
 
 const envUrl = GCF_URL
 
@@ -37,11 +38,13 @@ export const useUserStore = defineStore("user", () => {
     const web3 = new Web3(Web3.givenProvider)
 
     userState.wallet = (await web3.eth.requestAccounts())[0];
+
     await getUserBalance(userState.wallet);
   }
 
   const getUserBalance = async (wallet?: string) => {
     if (!(isConnected && wallet)) return console.error('USER NOT CONNECTED, CANT GET BALANCE');
+
     const endpoint = `${ envUrl }/${ wallet }`;
 
     try {
