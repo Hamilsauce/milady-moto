670caa8cc942af9d5ccdf17bacf4bb5a361d4049
jake hamilton <jacobwilsonhamilton@gmail.com>
2022-12-26 22:27:44 -0600

    vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "http://localhost:5173/milady-moto/",
+  base: "/milady-moto/",
   plugins: [vue()],
   resolve: {
     alias: {
